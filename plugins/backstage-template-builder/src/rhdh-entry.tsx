ad5da0947cec8b27a6790531467b5d748de8b2a6
Balaji Siva <basivasu@redhat.com>
2026-02-02 12:05:32 -0800

    Test: absolute minimal component

--- plugins/backstage-template-builder/src/rhdh-entry.tsx
+++ plugins/backstage-template-builder/src/rhdh-entry.tsx
@@ -1,33 +1,7 @@
-// RHDH Dynamic Plugin Entry Point
+// RHDH Dynamic Plugin Entry Point - Absolute minimum
 import React from 'react';
 
-// Component definition
-const TestComponent = () => {
-  return React.createElement(
-    'div',
-    {
-      style: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        fontFamily: 'sans-serif',
-        backgroundColor: '#18181b',
-        color: '#fff'
-      }
-    },
-    React.createElement(
-      'div',
-      { style: { textAlign: 'center', padding: '2rem' } },
-      React.createElement('h1', { style: { fontSize: '2rem', marginBottom: '1rem' } }, '🎉 Template Builder!'),
-      React.createElement('p', { style: { color: '#a1a1aa' } }, 'RHDH plugin loaded'),
-      React.createElement('p', { style: { color: '#a1a1aa', marginTop: '1rem' } }, 'Test version')
-    )
-  );
-};
-
-// CRITICAL: Export the component itself, not the result of calling it
-export const PluginRoot = TestComponent;
-
-// Also try default export
-export default TestComponent;
+// Simplest possible component - just text
+export function PluginRoot() {
+  return React.createElement('h1', null, 'IT WORKS!');
+}
